174fc3bd2983884f9cbe8fc23d5311ba07941608
Kim Persson <kimpersson88@gmail.com>
2022-02-08 17:33:11 +0100

    chore: Remove unnecessary dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,7 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
 
 dotenv.config();
 
